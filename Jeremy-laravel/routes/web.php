fe10807567abab209b066d949e118c86fd3535a4
infomotin <motin.mmk.jr@gmail.com>
2025-09-14 15:19:35 +0600

    FOR LOCAL FILE UPDATE

--- Jeremy-laravel/routes/web.php
+++ Jeremy-laravel/routes/web.php
@@ -42,25 +42,38 @@ Route::get('/datainput', function () {
     // }
 
 
-   $genaratedeRow = function ($row) {
-       return [
-           'log_date' => date('Y-m-d', strtotime($row[0])),
-           'log_time' => $row[1],
-           'gauge_one_reading' => $row[2],
-           'gauge_two_reading' => $row[3],
-           'gauge_one_temperature' => $row[4],
-           'gauge_two_temperature' => $row[5],
-       ];
-   };
+//    $genaratedeRow = function ($row) {
+//        return [
+//            'log_date' => date('Y-m-d', strtotime($row[0])),
+//            'log_time' => $row[1],
+//            'gauge_one_reading' => $row[2],
+//            'gauge_two_reading' => $row[3],
+//            'gauge_one_temperature' => $row[4],
+//            'gauge_two_temperature' => $row[5],
+//        ];
+//    };
 
 
-   DB::statement('SET foreign_key_checks = 0');
-   DB::statement('ALTER TABLE gauge_readings DISABLE KEYS');
-   foreach (App\Helpers\ArrayHelper::chunkFileAnother($file_path, $genaratedeRow, 1000) as $chunk) {
-       GaugeReading::insert($chunk);
-   }
-   DB::statement('SET foreign_key_checks = 1');
-   DB::statement('ALTER TABLE gauge_readings ENABLE KEYS');
+//    DB::statement('SET foreign_key_checks = 0');
+//    DB::statement('ALTER TABLE gauge_readings DISABLE KEYS');
+//    foreach (App\Helpers\ArrayHelper::chunkFileAnother($file_path, $genaratedeRow, 1000) as $chunk) {
+//        GaugeReading::insert($chunk);
+//    }
+//    DB::statement('SET foreign_key_checks = 1');
+//    DB::statement('ALTER TABLE gauge_readings ENABLE KEYS');
+    $escaped_file_path = DB::getPdo()->quote($file_path);
+    DB::statement("
+       LOAD DATA LOCAL INFILE $escaped_file_path
+       INTO TABLE gauge_readings
+       FIELDS TERMINATED BY ','
+       ENCLOSED BY '\"'
+       LINES TERMINATED BY '\n'
+       (@dummy, log_time, gauge_one_reading, gauge_two_reading, gauge_one_temperature, gauge_two_temperature)
+       SET log_date = STR_TO_DATE(@dummy, '%Y-%m-%d');
+    ");
+
+
+
    echo "<h1 class='text-center text-2xl background-green-500 text-white'>Success</h1>";
 });
 
